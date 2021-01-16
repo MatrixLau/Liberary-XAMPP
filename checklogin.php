a2b12fe0a6e467e28ec257a607f47ee3236067a0
MatrixLau <41422920+MatrixLau@users.noreply.github.com>
2021-01-16 21:03:57 +0800

    add comments

--- checklogin.php
+++ checklogin.php
@@ -1,10 +1,10 @@
 <?php
-    header("Content-Type:text/html;charset=utf-8");
+    header("Content-Type:text/html;charset=utf-8");       //设置编码
     require_once"conn.php";
-    $query = "select * from user where status=1";
+    $query = "select * from user where status=1";       //user为库中管理员的表 status是登陆状态
     mysqli_query($conn,$query);
     $result = mysqli_query($conn,$query);
-    if($result->num_rows <= 0){
+    if($result->num_rows <= 0){                //result->num_rows要是>0就是有结果 就等于已经有管理登录了就不用重新登录 否则要登陆
         ?>
         <script>
             alert("请登录！");
--- conn.php
+++ conn.php
@@ -1,9 +1,10 @@
 <?php
-    $conn = mysqli_connect("localhost:3316","root","0113","liberary");
+    $conn = mysqli_connect("localhost:3316","root","0113","liberary");        //值依次为mySQL服务器地址、账号名、密码、数据库名
     if(!$conn){
         echo "连接失败：".mysqli_connect_error();
         exit();
     }
-    mysqli_set_charset($conn,"UFT8");
+    //设置设置编码
+    mysqli_set_charset($conn,"UFT8");                    
     mysqli_query($conn,"SET NAMES utf8");
 ?>
